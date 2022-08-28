e1f2bceebd83d4b5d796cb0626e031338347a119
Christian Lisangola <jochri3@gmail.com>
2022-08-28 17:27:51 +0300

    handle 404

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.get("/api/categories",(req,res)=>{
 app.get("/api/categories/:id",(req,res)=>{
     const id=parseInt(req.params.id)
     const category=categories.find(category=>category.id===id);
+    if(!category) return res.status(404).send("Category with given id does not exist")
     res.send(category)
 })
 
@@ -23,6 +24,7 @@ app.get("/api/categories/:id",(req,res)=>{
 app.delete("/api/categories/:id",(req,res)=>{
     const id=parseInt(req.params.id)
     const index=categories.findIndex(category=>category.id===id);
+    if(index===-1) return res.status(404).send("Category with given id does not exist")
     categories.splice(index,1)
     res.send("Supprimé avec succès")
 })
