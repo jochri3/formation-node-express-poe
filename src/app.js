59e7c21d024e09eaf37a5e613dd6a1d91913f9b9
Christian Lisangola <jochri3@gmail.com>
2022-08-28 17:24:05 +0300

    Delete ressource

--- src/app.js
+++ src/app.js
@@ -5,18 +5,26 @@ const app=express();
 
 app.use(cors())
 
+const categories=[{id:1,nom:"Jeux videos"},{id:2,nom:"Ordinateurs portables"}]
+
 
 
 app.get("/api/categories",(req,res)=>{
-    // res.send("Salut tout le monde")
-    const data=[{id:1,nom:"Jeux videos",id:2,nom:"Ordinateurs portables"}]
-    res.send(data)
+    res.send(categories)
 })
 
 app.get("/api/categories/:id",(req,res)=>{
-    // res.send("Salut tout le monde")
-    console.log("Id : ",req.params.id)
-    res.send(`L'id envoyé est ${req.params.id}`)
+    const id=parseInt(req.params.id)
+    const category=categories.find(category=>category.id===id);
+    res.send(category)
+})
+
+
+app.delete("/api/categories/:id",(req,res)=>{
+    const id=parseInt(req.params.id)
+    const index=categories.findIndex(category=>category.id===id);
+    categories.splice(index,1)
+    res.send("Supprimé avec succès")
 })
 
 
