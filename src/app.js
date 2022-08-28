54186dbc3407dac21e197b64153fa67a46d65c4b
Christian Lisangola <jochri3@gmail.com>
2022-08-28 17:12:36 +0300

    add basic get route handler

--- src/app.js
+++ src/app.js
@@ -3,6 +3,14 @@ const express=require("express");
 const app=express();
 
 
+
+app.get("/api/categories",(req,res)=>{
+    // res.send("Salut tout le monde")
+    const data=[{id:1,nom:"Jeux videos",id:2,nom:"Ordinateurs portables"}]
+    res.send(data)
+})
+
+
 app.listen(3000,()=>{
     console.log("Le serveur écoute sur le port 3000!!")
 })
